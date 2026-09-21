0a049059c59d52591b30aa41920fd09ec3e91de9
Mahek Tarpara <mahektarpara016@gmail.com>
2026-09-21 08:46:58 +0530

    onefile

---
+++ 1.py
@@ -0,0 +1,63 @@
+#1. Variables#
+
+"1. Create variables to store name, age, and city and display them."
+
+name="mahek"
+age=21
+city="rajkot"
+print("---------------program 1-----------------")
+print("name:",name)
+print("age:",age)
+print("city:",city)
+print("\n")
+
+"2. Swap the values of two variables."
+print("---------------program 2-----------------")
+
+a=5
+b=20
+
+a,b=b,a
+print("a1",a)
+print("b1",b)
+print("\n")
+
+"3. Calculate the area of a rectangle using variables."
+
+print("---------------program 3-----------------")
+
+length=10
+width=4
+
+area= length *width
+
+print("The area of a rectangle is:",area)
+print("\n")
+
+
+"4. Calculate simple interest using variables."
+print("---------------program 4-----------------")
+
+principal=2000
+rate=5
+time=2
+
+simple_intrest=(principal*rate*time)/100
+print("The simple intrest is:",simple_intrest)
+print("\n")
+
+
+
+"5. Convert Celsius temperature to Fahrenheit."
+print("---------------program 5-----------------")
+celsius=25
+fahrenheit=(celsius*9/5)+32
+print("Temperature in fahrenheit:",fahrenheit)
+
+
+
+
+print("\n")
+
+
+
