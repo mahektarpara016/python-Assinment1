810f7c1c2d234948d5e8366db2f0c040451aa965
Mahek Tarpara <mahektarpara016@gmail.com>
2026-09-21 08:46:58 +0530

    added 2

---
+++ 2.py
@@ -0,0 +1,81 @@
+
+#1. Demonstrate int, float, str, bool, and complex.
+print("---------------program 1-----------------")
+
+
+a=10
+b=10.2
+c="Hello"
+d=True
+e=3+4j
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+print(d)
+print(type(d))
+print(e)
+print(type(e))
+
+
+# 2. Accept two numbers and display their data types.
+print("---------------program 2-----------------")
+
+
+'''a=int(input("enter number 1:"))
+b=int(input("enter number 2:"))
+
+print("First Number:",a)
+print("data type is:",type(a))
+
+print("Second Number:",b)
+print("data type is:",type(b))'''
+
+
+# 3. Convert a string number into an integer and float.
+print("---------------program 3-----------------")
+
+
+a="10"
+b="10.10"
+
+x=int(a)
+print(type(x))
+
+y=float(b)
+print(type(y))
+
+
+# 4.find length of string.
+print("---------------program 4-----------------")
+
+
+a=input("Enter a String:")
+print("Length of your string is:",len(a))
+
+
+# 5. Create a list, tuple, set, and dictionary and display their types.
+print("---------------program 5-----------------")
+
+a=[10,20,30]
+
+b=(100,200,300)
+
+c={1,2,3}
+
+d={"name":"mahek","age":20}
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+print(c)
+print(type(c))
+
+print(d)
+print(type(d))
+
